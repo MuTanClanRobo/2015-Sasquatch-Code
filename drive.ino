8a0faa14299c0294908a97e3091bb61259223a9d
MuTanClanRobo <jsdcrobo17@gmail.com>
2015-03-12 19:50:25 -0500

    Update drive.ino

--- drive.ino
+++ drive.ino
@@ -20,8 +20,8 @@ ROPWM lf_wheel_pwm(0);
 ROPWM rf_wheel_pwm(1);              
 ROPWM lr_wheel_pwm(2);
 ROPWM rr_wheel_pwm(3);
-ROPWM pwm4(4);
-ROPWM pwm5(5);
+ROPWM conv_pwm(4);
+ROPWM shoot_pwm(5);
 RODigitalIO dig0Out(0,OUTPUT);
 RODigitalIO dig1Out(1,OUTPUT);
 RODigitalIO dig2Out(2,OUTPUT);
@@ -36,39 +36,39 @@ int x, y, rotate;
 DRIVEMODE current_mode = MECANUM;
 int lf,rf,lr,rr;
 
-void regulate_pressure(){
-
-}
-
-void jump_front_wheels(DRIVEMODE mode){
-    // @TODO Fill this out with Collin
-}
-
-void jump_rear_wheels(DRIVEMODE mode){
-    // @TODO Fill this out with Collin
-}
-
-void switch_mode(DRIVEMODE mode){
-  jump_rear_wheels(mode);
-  jump_front_wheels(mode);
-  current_mode = mode;
-}
-
 void setup()
 {
   /* initiate comms */
   RobotOpen.begin(&enabled,&disabled, &timedtasks);
 }
 
-void set_mecanum_throttle_values(){
-  lf = (x + y + rotate)/wheel_speed_reduction;
-  rf = (x - y + rotate)/wheel_speed_reduction;
+inline void regulate_pressure(){
+
+}
+
+void jump_front_wheels(DRIVEMODE mode){
+  // @TODO Fill this out with Collin
+}
+
+void jump_rear_wheels(DRIVEMODE mode){
+  // @TODO Fill this out with Collin
+}
+
+inline void switch_mode(DRIVEMODE mode){
+  jump_rear_wheels(mode);
+  jump_front_wheels(mode);
+  current_mode = mode;
+}
+
+inline void set_mecanum_throttle_values(){
+  lf = ( x + y + rotate)/wheel_speed_reduction;
+  rf = ( x - y + rotate)/wheel_speed_reduction;
   lr = (-x + y + rotate)/wheel_speed_reduction;
   rr = (-x - y + rotate)/wheel_speed_reduction;
 }
 
-void set_tank_throttle_values(){
-  int rotaion_coefficient = 1;
+inline void set_tank_throttle_values(){
+  int rotation_coefficient = 1;
   int rotation_offset = rotate/rotation_coefficient;
   lf = (+y + rotation_offset)/wheel_speed_reduction;
   rf = (-y + rotation_offset)/wheel_speed_reduction;
@@ -76,7 +76,7 @@ void set_tank_throttle_values(){
   rr = (-y + rotation_offset)/wheel_speed_reduction;
 }
 
-void normalize_wheel_throttles(){
+inline void normalize_wheel_throttles(){
   int maximum = max(max(abs(lf), abs(rf)), max(abs(lr), abs(rr)));
   if (maximum > 127) {
     lf = (lf / maximum) * 127;
@@ -86,47 +86,61 @@ void normalize_wheel_throttles(){
   }
 }
 
-void enabled() {
-  // get desired translation and rotation, scaled to [-127..128] (0 neutral)
-  x = usb1.leftX() - 127;
-  y = (255 - usb1.leftY()) - 127;
-  rotate = usb1.rightX() - 127;
-  
-  // calculate wheel throttles
-  if (current_mode == MECANUM)
-    set_mecanum_throttle_values();
-  else
-    set_tank_throttle_values();
-    
-  normalize_wheel_throttles();
-  
+inline void pwn_write_throttle_values(){
   // Set PWMs, shifted back to [0..255]
   lf_wheel_pwm.write(lf + 127);
   rf_wheel_pwm.write(rf + 127);
   lr_wheel_pwm.write(lr + 127);
   rr_wheel_pwm.write(rr + 127);
+}
+
+inline void pwm_write_conv(){
+  int conv = usb1.lTrigger()/2;
+  if (usb1.btnLShoulder())
+    conv_pwm.write(-conv);
+  else
+    conv_pwm.write(conv);    
+}
+
+inline void pwm_write_shoot(){
+  int shoot = usb1.lTrigger()/2;
+  if (usb1.btnLShoulder())
+    shoot_pwm.write(-shoot);
+  else
+    shoot_pwm.write(shoot); 
+}
+
+void enabled() {
+  // get desired translation and rotation, scaled to [-127..128] (0 neutral)
+  x = usb1.leftX() - 127;
+  y = (255 - usb1.leftY()) - 127;
+  rotate = usb1.rightX() - 127;
+
+  // calculate wheel throttles
+  if (current_mode == MECANUM)
+    set_mecanum_throttle_values();
+  else
+    set_tank_throttle_values();
+
+  normalize_wheel_throttles();
+  pwn_write_throttle_values();
+  
+  pwm_write_conv();
+  pwm_write_shoot();
 
   // WHAT THE FLYING FUCK IS GOING ON???!!!
   if (usb1.btnY()){
     dig0Out.off();
     dig1Out.off();
     dig2Out.on();
-    RODashboard.debug("sensor high");}
-    else{
+    RODashboard.debug("sensor high");
+  }
+  else{
     dig0Out.off();
     dig1Out.off();
     dig2Out.off();
-    RODashboard.debug("sensor low");}
-  
-  if(usb1.btnA())
-    sol0.on();
-  else
-    sol0.off();
-  if(usb1.btnB())
-    sol1.on();
-  else
-    sol1.off();
-    
+    RODashboard.debug("sensor low");
+  }
   RODashboard.publish("sensorinput", PressureSensor.read());
 }
 
@@ -147,7 +161,6 @@ void timedtasks() {
   RODashboard.publish("Uptime Seconds", ROStatus.uptimeSeconds());
   RODashboard.publish("wheel_speed_reduction", wheel_speed_reduction);
 }
-
 
 // !!! DO NOT MODIFY !!!
 void loop() {
